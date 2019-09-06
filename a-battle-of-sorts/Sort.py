d70ff7ddd8b02352f54650b7f81f9251ee000d46
Maxwell Ward <maxward4@gmail.com>
2019-09-06 17:18:01 -0400

    function almost working

--- a-battle-of-sorts/Sort.py
+++ a-battle-of-sorts/Sort.py
@@ -1,27 +1,31 @@
-def insertion_sort(arr):
-  for k in range(1, len(arr)):
-    cur = arr[k]
-    j = k
-    while j > 0 and arr[j-1] > cur:
-      arr[j] = arr[j-1]
-      j = j - 1
-    arr[j] = cur
+# def insertion_sort(arr):
+#     for k in range(1, len(arr)):
+#         cur = arr[k]
+#         j = k
+#         while j > 0 and arr[j-1] > cur:
+#             arr[j] = arr[j-1]
+#             j = j - 1
+#         arr[j] = cur
 
-a = [4,-2,8,-1]
-insertion_sort(a)
+
+# a = [4, -2, 8, -1]
+# insertion_sort(a)
+# print("a is")
+# print(a)
+
 
 def selection_sort(arr):
-  for current_index in range(len(arr)):
-    minimum_remaining_card_index = arr.index(min(arr[current_index,len(arr)]))
-    if current_index == minimum_remaining_card_index:
-      continue
-    else:
-      # swap cards
-      alias = minimum_remaining_card_index
-      minimum_remaining_card_index = arr[current_index]
-      current_index = arr[alias]
-
-a = [4,-2,8,-1]
-selection_sort(a)
+    for k in range(len(arr)):
+        cur = arr[k]
+        j = k
+        min_rem = arr.index(min(arr[j:-1]))
+        print("min_rem is ", min_rem)
+        while j < len(arr) and cur > arr[min_rem]:
+          arr[j] = arr[min_rem]
+          min_rem = cur
 
 
+b = [4, -2, 8, -1]
+selection_sort(b)
+print("b is")
+print(b)
