9b96a6b75be4582f94a7c48d38ca1e052fbc62f1
Maxwell Ward <maxward4@gmail.com>
2019-09-08 00:05:12 -0400

    selection sort functional now

--- a-battle-of-sorts/Sort.py
+++ a-battle-of-sorts/Sort.py
@@ -1,31 +1,36 @@
-# def insertion_sort(arr):
-#     for k in range(1, len(arr)):
-#         cur = arr[k]
-#         j = k
-#         while j > 0 and arr[j-1] > cur:
-#             arr[j] = arr[j-1]
-#             j = j - 1
-#         arr[j] = cur
-
-
-# a = [4, -2, 8, -1]
-# insertion_sort(a)
-# print("a is")
-# print(a)
+def insertion_sort(arr):
+    for k in range(1, len(arr)):
+        cur = arr[k]
+        j = k
+        while j > 0 and arr[j - 1] > cur:
+            arr[j] = arr[j - 1]
+            j = j - 1
+        arr[j] = cur
 
 
 def selection_sort(arr):
-    for k in range(len(arr)):
+    for k in range(len(arr)-1):
         cur = arr[k]
         j = k
-        min_rem = arr.index(min(arr[j:-1]))
-        print("min_rem is ", min_rem)
-        while j < len(arr) and cur > arr[min_rem]:
-          arr[j] = arr[min_rem]
-          min_rem = cur
+        cur_min = cur
+        # while loop to iterate through array in order to find the minimum
+        while j < len(arr)-1:
+            next_card = arr[j+1]
+            if cur_min > next_card:
+                cur_min = next_card
+            j += 1
+        # replace current card with the minimum, unless they are the same number/card
+        if cur_min != cur:
+            swap_ind = arr.index(cur_min)
+            arr[k] = cur_min
+            arr[swap_ind] = cur
 
 
-b = [4, -2, 8, -1]
-selection_sort(b)
-print("b is")
-print(b)
+a = [4, -2, 8, -1]
+selection_sort(a)
+print(a)
+
+
+# if __name__ == '__main__':
+# main section here,
+# within this big conditional
